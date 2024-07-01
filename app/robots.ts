c8b2c88f7bdf02bb68936c8d81f21c80ebdbbecd
tricky <trickya23@gmail.com>
2024-07-01 04:29:31 +0000

    sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    "sitemap": "https://ambrosetogobo.com/sitemap.xml",
+    "sitemap": "https://ambrosetogobo.com/sitemap",
     "host": "https://ambrosetogobo.com",
     "rules": [
       {
